44cc627ad4705fa7083b750a5c89a85487be8f8f
tertek <ekin.tertemiz@swisstph.ch>
2021-10-20 09:24:48 +0200

    php 7.3 compatibility.

--- classes/parts.class.php
+++ classes/parts.class.php
@@ -3,8 +3,8 @@ namespace STPH\addressAutoComplete;
 
 
 class parts {
-    public string $street;
-    public string $no;
-    public string $code;
-    public string $place;
+    public $street;
+    public $no;
+    public $code;
+    public $place;
 }
